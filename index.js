df9cca1d81dae1b6bd85c54f005594f3dcc9810b
Shadrack kipkorir <shadybett540@gmail.com>
2023-09-30 19:49:05 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+document.getElementById('main').remove();
+
+let newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.textContent = 'Shadrack Kipkorir is the champion';
+
+document.body.appendChild(newHeader);
+
+
+
+
+
+
